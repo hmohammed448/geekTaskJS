e307228ee4401c861b0b71bbb037a4c732d5f14f
Mohammed Hussain <hmohammed448@gmail.com>
2024-01-17 19:53:10 +0530

    changes

---
+++ js/sticky_notes.js
@@ -0,0 +1,24 @@
+// taking input of text area and color
+let textArea = document.getElementById("textPicker");
+let color = document.getElementById("colorPicker");
+
+
+let parentEle = document.getElementById("notesEle");
+let trigger = document.getElementById("btn");
+trigger.addEventListener("click", (e) => {
+    if (textArea.value === "") {
+        alert("Enter Some Text");
+        return;
+    }
+
+    document.getElementById("blankP").style.display = "none";
+
+    let childEle = document.createElement("div"); // to store div 
+    childEle.classList.add("divChildBox"); // attribute
+    childEle.innerHTML = `<p>${textArea.value}</p>`; // paragraph into div
+    childEle.style.backgroundColor = color.value;
+
+    parentEle.appendChild(childEle);
+    textArea.value = "";
+})
+
