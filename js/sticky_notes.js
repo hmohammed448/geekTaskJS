019c312d1a506e81640e9e542013b7d64379ab9a
Mohammed Hussain <hmohammed448@gmail.com>
2024-01-18 02:13:35 +0530

    changes

--- js/sticky_notes.js
+++ js/sticky_notes.js
@@ -13,15 +13,28 @@ trigger.addEventListener("click", (e) => {
         alert("Enter Some Text");
         return;
     }
-
     document.getElementById("blankP").style.display = "none";
 
-    let childEle = document.createElement("div"); // to store div 
-    childEle.classList.add("divChildBox"); // attribute
-    childEle.innerHTML = `<p>${textArea.value}</p>`; // paragraph into div
-    childEle.style.backgroundColor = color.value;
+    let card = document.createElement("div"); // to store div 
+    card.classList.add("divChildBox"); // attribute
+    card.style.backgroundColor = color.value;
 
-    parentEle.appendChild(childEle);
+    // paragraph into div
+    let p = document.createElement("p");
+    p.innerText = textArea.value;
+    card.appendChild(p);
+
+    // for creating X btn on list
+    let closeBtn = document.createElement("i");
+    closeBtn.classList.add("material-symbols-outlined");
+    closeBtn.innerText = " close ";
+    card.appendChild(closeBtn);
+
+    // close to remove btn
+    closeBtn.addEventListener("click", (e) => {
+        closeBtn.parentElement.remove();
+    });
+
+    parentEle.appendChild(card);
     textArea.value = "";
 });
-
