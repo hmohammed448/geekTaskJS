5c051bd4481e341a283f28dc87b477ada8ed9121
Mohammed Hussain <hmohammed448@gmail.com>
2024-01-19 17:09:46 +0530

    search functioning

--- Emoji_Project/emoji.js
+++ Emoji_Project/emoji.js
@@ -20,7 +20,6 @@ function displayResult() {
 
         // // Set innerText for td elements
         newTd1.innerText = e.emoji;
-        newTd1.style.width = "200px";
         newTd2.innerText = printString;
         newTd3.innerText = e.description;
 
@@ -32,13 +31,33 @@ function displayResult() {
 
         tab.appendChild(newRow);
 
-
-
     })
-
-
-
-
 }
 
 window.onload = () => displayResult();
+
+// input trigger function
+const inputTriger = () => {
+    let fetchText = document.getElementById("inputSearch").value.toUpperCase();
+    console.log(fetchText.value);
+
+    // let table = document.getElementById("table");
+    let everyRow = document.querySelectorAll("tr");
+    console.log(everyRow);
+
+    everyRow.forEach(item => console.log(item));
+
+    for (let i = 0; i < everyRow.length; i++) {
+        let td = everyRow[i].getElementsByTagName("td")[1];
+        console.log(everyRow[i]);
+        // if (td) {
+        let listValue = td.textContent || td.innerHTML;
+
+        if (listValue.toUpperCase().indexOf(fetchText) > -1) {
+            everyRow[i].style.display = "";
+        } else {
+            everyRow[i].style.display = "none";
+        }
+        // }
+    }
+};
