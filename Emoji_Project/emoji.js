62354e437fedcd5b307b19905093b2d6b2a009a3
Mohammed Hussain <hmohammed448@gmail.com>
2024-01-19 15:46:14 +0530

    changes

--- Emoji_Project/emoji.js
+++ Emoji_Project/emoji.js
@@ -9,26 +9,31 @@ function displayResult() {
         let newRow = document.createElement("tr");
         newRow.style.fontSize = "2rem";
 
-        // let val = e.aliases;
-        // let newVal = JSON.stringify(val);
-        // console.log(newVal.slice(2,-2));
+        let val = e.aliases;
+        let str = e.aliases.map(item => item.replaceAll("_", " "))
+        let printString = str.toString();
+
         // creating td elements
         let newTd1 = document.createElement("td");
         let newTd2 = document.createElement("td");
         let newTd3 = document.createElement("td");
 
-        // Set innerText for td elements
+        // // Set innerText for td elements
         newTd1.innerText = e.emoji;
-        newTd2.innerText = e.aliases;
+        newTd1.style.width = "100px";
+        newTd2.innerText = printString;
         newTd3.innerText = e.description;
 
-        // Append td elements to tr element
+        // // Append td elements to tr element
         newRow.appendChild(newTd1);
         newRow.appendChild(newTd2);
         newRow.appendChild(newTd3);
         newRow.style.margin = " 5px 0px";
 
         tab.appendChild(newRow);
+
+
+        
     })
 
 
