2173c5ae297142b7f9153d3cb19fdd447f624a27
Mohammed Hussain <hmohammed448@gmail.com>
2024-03-14 12:18:13 +0530

    XHR

--- js/weekly_test3.js
+++ js/weekly_test3.js
@@ -3,10 +3,52 @@ const proPrice1 = document.getElementById("prod_1_price");
 
 let cartDiv = document.querySelector("#cartDiv");
 
-let productsList = [];
+let productsList = [
+  {
+    naam: "Products-1",
+    quantity: 0,
+    price: 100,
+  },
+  {
+    naam: "Products-2",
+    quantity: 0,
+    price: 200,
+  },
+  {
+    naam: "Products-3",
+    quantity: 0,
+    price: 300,
+  },
+];
+
+let productDiv = document.querySelector(".productDiv");
+
+window.addEventListener("load", () => {
+  productsList.forEach(function (product) {
+    const div = document.createElement("div");
+    div.classList.add("card");
+    div.innerHTML = `<p class="prodName">${product.naam}</p>
+  <p class="price">${product.price}</p>
+  <p class="btn">
+    <i class="material-symbols-outlined remove"> remove </i>
+    <span id="prodCount">${product.quantity}</span>
+    <i class="material-symbols-outlined add"> add </i>
+  </p>`;
+
+    productDiv.append(div);
+  });
+});
 
 // Universal btn
 let btns = document.querySelectorAll(".btn");
+
+// console.log(productDiv);
+productDiv.addEventListener("click", function (e) {
+  // console.log(e.target.classList.contains("add"));
+  if (e.target.classList.contains("add")) {
+  } else if (e.target.classList.contains("remove")) {
+  }
+});
 
 // Iterate over each button and add event listeners
 btns.forEach((btn) => {
@@ -40,7 +82,6 @@ btns.forEach((btn) => {
         price: productPrice,
       });
 
-      // Update cart display
       renderCart();
     }
 
@@ -63,12 +104,14 @@ function renderCart() {
 
   // Iterate over productsList and add each product to the cart display
   productsList.forEach((product, index) => {
-    let cartItem = document.createElement("div");
-    cartItem.classList.add("card");
-    cartItem.innerHTML = `
+    if (product.quantity > 0) {
+      let cartItem = document.createElement("div");
+      cartItem.classList.add("card");
+      cartItem.innerHTML = `
       <p>${product.naam}</p>
-      <p>${product.quantity}</p> x <span>${product.price}</span>
-    `;
-    cartDiv.appendChild(cartItem);
+      <p class="qty">${product.quantity}</p> x <span>${product.price}</span>
+      `;
+      cartDiv.appendChild(cartItem);
+    }
   });
 }
