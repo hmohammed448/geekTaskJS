3376ca8ac48513aa33f66ec8356f484fd3f49113
Mohammed Hussain <hmohammed448@gmail.com>
2024-04-26 18:13:25 +0530

    RMH

---
+++ ht/footer.blade.php
@@ -0,0 +1,222 @@
+
+<p>Best regards,</p>
+
+<p>SellMyHome Team</p>
+<div>
+    <table class="x_-746680097MsoNormalTable" border="0" cellspacing="0" cellpadding="0"
+        style="border-collapse: collapse;">
+        <tbody>
+            <tr>
+                <td style="padding: 0.0cm 0.0cm 0.0cm 0.0cm;">
+                    <table class="x_-746680097MsoNormalTable" border="0" cellspacing="0" cellpadding="0">
+                        <tbody>
+                            <tr>
+                                <td style="padding: 0.0cm 19.5pt 0.0cm 0.0cm;">
+                                    <div align="center">
+                                        <table class="x_-746680097MsoNormalTable" border="0" cellspacing="0"
+                                            cellpadding="0" style="border-collapse: collapse;">
+                                            <tbody>
+                                                <tr>
+                                                    <td style="padding: 0.0cm 0.75pt 14.25pt 0.0cm;">
+                                                        <p class="MsoNormal" style="margin: 0.75pt;"><img width="188"
+                                                                height="278" style="width: 1.9583in;height: 2.8916in;"
+                                                                src="https://zmdownload-accl.zoho.eu/zm/ImageDisplay?na=3343243000000002002&amp;nmsgId=1676482073010030001&amp;f=1.png&amp;mode=inline&amp;cid=image001.png@01D94162.D145BFF0&amp;">
+                                                        </p>
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                    <p class="MsoNormal" align="center" style="text-align: center;"><span
+                                            style="display: none;">&nbsp;</span></p>
+                                    <div align="center">
+                                        <table class="x_-746680097MsoNormalTable" border="0" cellspacing="0"
+                                            cellpadding="0" style="border-collapse: collapse;">
+                                            <tbody>
+                                                <tr>
+                                                    <td style="padding: 0.0cm 0.75pt 0.0cm 0.0cm;"></td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                    <p class="MsoNormal" align="center" style="text-align: center;"></p>
+                                </td>
+                                <td
+                                    style="border: none;border-right: solid rgb(33,114,98) 1.5pt;padding: 0.75pt 0.0cm 0.0cm 0.0cm;">
+                                </td>
+                                <td style="padding: 0.0cm 0.75pt 0.0cm 19.5pt;">
+                                    <table class="x_-746680097MsoNormalTable" border="0" cellspacing="0" cellpadding="0"
+                                        style="border-collapse: collapse;">
+                                        <tbody>
+                                            
+                                            <tr>
+                                                <td style="padding: 0.0cm 0.75pt 0.0cm 0.0cm;">
+                                                    <table class="x_-746680097MsoNormalTable" border="0" cellspacing="0"
+                                                        cellpadding="0" style="border-collapse: collapse;">
+                                                        <tbody>
+                                                            
+                                                            <tr>
+                                                                <td style="padding: 0.75pt 3.75pt 0.75pt 0.0cm;">
+                                                                    <p class="MsoNormal" style="margin: 0.75pt;"><img
+                                                                            border="0" width="18" height="18"
+                                                                            style="width: 0.1916in;height: 0.1916in;"
+                                                                            src="https://zmdownload-accl.zoho.eu/zm/ImageDisplay?na=3343243000000002002&amp;nmsgId=1676482073010030001&amp;f=3.png&amp;mode=inline&amp;cid=image003.png@01D94162.D145BFF0&amp;">
+                                                                    </p>
+                                                                </td>
+                                                                <td nowrap=""
+                                                                    style="padding: 0.75pt 0.0cm 0.75pt 0.0cm;">
+                                                                    <p class="MsoNormal"
+                                                                        style="margin: 0.75pt;line-height: 12.0pt;">
+                                                                        <span
+                                                                            style="font-size: 10.0pt;font-family: Tahoma, sans-serif;color: rgb(108,117,124);"><a
+                                                                                href="tel:01162291021"
+                                                                                target="_blank"><span
+                                                                                    style="color: rgb(108,117,125);">0116
+                                                                                    229 1021</span></a></span></p>
+                                                                </td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td style="padding: 0.75pt 3.75pt 0.75pt 0.0cm;">
+                                                                    <p class="MsoNormal" style="margin: 0.75pt;"><img
+                                                                            border="0" width="18" height="18"
+                                                                            style="width: 0.1916in;height: 0.1916in;"
+                                                                            src="https://zmdownload-accl.zoho.eu/zm/ImageDisplay?na=3343243000000002002&amp;nmsgId=1676482073010030001&amp;f=3.png&amp;mode=inline&amp;cid=image003.png@01D94162.D145BFF0&amp;">
+                                                                    </p>
+                                                                </td>
+                                                                <td nowrap=""
+                                                                    style="padding: 0.75pt 0.0cm 0.75pt 0.0cm;">
+                                                                    <p class="MsoNormal"
+                                                                        style="margin: 0.75pt;line-height: 12.0pt;">
+                                                                        <span
+                                                                            style="font-size: 10.0pt;font-family: Tahoma, sans-serif;color: rgb(108,117,124);"><a
+                                                                                href="tel:02038756999"
+                                                                                target="_blank"><span
+                                                                                    style="color: rgb(108,117,125);">020
+                                                                                    3875 6999</span></a></span></p>
+                                                                </td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td valign="top"
+                                                                    style="padding: 0.75pt 3.75pt 0.75pt 0.0cm;">
+                                                                    <p class="MsoNormal" style="margin: 0.75pt;"><img
+                                                                            border="0" width="18" height="18"
+                                                                            style="width: 0.1916in;height: 0.1916in;"
+                                                                            src="https://zmdownload-accl.zoho.eu/zm/ImageDisplay?na=3343243000000002002&amp;nmsgId=1676482073010030001&amp;f=4.png&amp;mode=inline&amp;cid=image004.png@01D94162.D145BFF0&amp;">
+                                                                    </p>
+                                                                </td>
+                                                                <td nowrap=""
+                                                                    style="padding: 0.75pt 0.0cm 0.75pt 0.0cm;">
+                                                                    <p class="MsoNormal"
+                                                                        style="margin: 0.75pt;line-height: 12.0pt;">
+                                                                        <span
+                                                                            style="font-size: 10.0pt;font-family: Tahoma, sans-serif;color: rgb(108,117,125);">61
+                                                                            Abbey Street, Leicester, LE1 3TE<br>
+                                                                            8 St Thomas Street, London, SE1
+                                                                            9RR</span><span
+                                                                            style="font-size: 10.0pt;font-family: Tahoma, sans-serif;color: rgb(108,117,124);"></span>
+                                                                    </p>
+                                                                </td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td style="padding: 0.75pt 3.75pt 0.75pt 0.0cm;">
+                                                                    <p class="MsoNormal" style="margin: 0.75pt;"><img
+                                                                            border="0" width="18" height="18"
+                                                                            style="width: 0.1916in;height: 0.1916in;"
+                                                                            src="https://zmdownload-accl.zoho.eu/zm/ImageDisplay?na=3343243000000002002&amp;nmsgId=1676482073010030001&amp;f=5.png&amp;mode=inline&amp;cid=image005.png@01D94162.D145BFF0&amp;">
+                                                                    </p>
+                                                                </td>
+                                                                <td nowrap=""
+                                                                    style="padding: 0.75pt 0.0cm 0.75pt 0.0cm;">
+                                                                    <p class="MsoNormal"
+                                                                        style="margin: 0.75pt;line-height: 12.0pt;">
+                                                                        <b><span
+                                                                                style="font-size: 10.0pt;font-family: Tahoma, sans-serif;color: rgb(33,114,97);"><a
+                                                                                    href="https://rentmyhome.co.uk/"
+                                                                                    target="_blank"><span
+                                                                                        style="color: rgb(33,114,98);">rentmyhome.co.uk</span></a></span></b>
+                                                                    </p>
+                                                                </td>
+                                                            </tr>
+                                                        </tbody>
+                                                    </table>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td style="padding: 0.0cm 0.75pt 0.0cm 0.0cm;">
+                                                    <table class="x_-746680097MsoNormalTable" border="0" cellspacing="0"
+                                                        cellpadding="0" style="border-collapse: collapse;">
+                                                        <tbody>
+                                                            <tr>
+                                                                <td width="30"
+                                                                    style="width: 22.5pt;padding: 14.25pt 0.75pt 0.0cm 0.0cm;">
+                                                                    <p class="MsoNormal" style="margin: 0.75pt;"><a
+                                                                            href="https://www.facebook.com/RMHINVALID"
+                                                                            target="_blank" rel="noreferrer"><span
+                                                                                style="font-size: 1.0pt;text-decoration: none;"><img
+                                                                                    border="0" width="30" height="30"
+                                                                                    style="width: 0.3083in;height: 0.3083in;"
+                                                                                    src="https://zmdownload-accl.zoho.eu/zm/ImageDisplay?na=3343243000000002002&amp;nmsgId=1676482073010030001&amp;f=6.png&amp;mode=inline&amp;cid=image006.png@01D94162.D145BFF0&amp;"></span></a><span
+                                                                            style="font-size: 1.0pt;"></span></p>
+                                                                </td>
+                                                                <td width="3"
+                                                                    style="width: 2.25pt;padding: 0.0cm 0.0cm 0.75pt 0.0cm;">
+                                                                </td>
+                                                                <td width="30"
+                                                                    style="width: 22.5pt;padding: 14.25pt 0.75pt 0.0cm 0.0cm;">
+                                                                    <p class="MsoNormal" style="margin: 0.75pt;"><a
+                                                                            href="https://uk.linkedin.com/company/rentmyhome-co-uk"
+                                                                            target="_blank"><span
+                                                                                style="font-size: 1.0pt;text-decoration: none;"><img
+                                                                                    border="0" width="30" height="30"
+                                                                                    style="width: 0.3083in;height: 0.3083in;"
+                                                                                    src="https://zmdownload-accl.zoho.eu/zm/ImageDisplay?na=3343243000000002002&amp;nmsgId=1676482073010030001&amp;f=7.png&amp;mode=inline&amp;cid=image007.png@01D94162.D145BFF0&amp;"></span></a><span
+                                                                            style="font-size: 1.0pt;"></span></p>
+                                                                </td>
+                                                                <td width="3"
+                                                                    style="width: 2.25pt;padding: 0.0cm 0.0cm 0.75pt 0.0cm;">
+                                                                </td>
+                                                            </tr>
+                                                        </tbody>
+                                                    </table>
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </td>
+                <td style="padding: 0.0cm 0.75pt 0.0cm 0.0cm;">
+                    <p class="MsoNormal">&nbsp;</p>
+                </td>
+            </tr>
+            <tr>
+                <td style="padding: 0.0cm 0.0cm 0.0cm 0.0cm;">
+                    <table class="x_-746680097MsoNormalTable" border="0" cellspacing="0" cellpadding="0" width="600"
+                        style="width: 450.0pt;border-collapse: collapse;">
+                        <tbody>
+                            <tr>
+                                <td style="padding: 17.25pt 0.75pt 0.0cm 0.0cm;">
+                                    <p class="MsoNormal" style="margin: 0.75pt;line-height: 9.75pt;"><span
+                                            style="font-size: 7.5pt;font-family: Tahoma, sans-serif;color: rgb(136,136,136);">The
+                                            content of this email is confidential and intended for the recipient
+                                            specified in message only. It is strictly forbidden
+                                            to share any part of this message with any third party, without a written
+                                            consent of the sender. If you received this message by mistake, please reply
+                                            to this message and follow with its deletion, so that we can ensure such a
+                                            mistake does not occur in the
+                                            future.</span></p>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </td>
+                <td style="padding: 0.0cm 0.75pt 0.0cm 0.0cm;">
+                    <p class="MsoNormal">&nbsp;</p>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+    <p class="MsoNormal">&nbsp;</p>
+</div>
